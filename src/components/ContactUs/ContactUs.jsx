9c5837feec70e54a2c3627300e7cdfbfa51d15f1
Umang <umangpachaury@gmail.com>
2025-04-26 16:58:24 +0530

    Added Following changes: Stopped Overflow of the site Aligned the Contact us and footer content Made Changes in client section

--- src/components/ContactUs/ContactUs.jsx
+++ src/components/ContactUs/ContactUs.jsx
@@ -3,28 +3,36 @@ import contactimg from "../../assets/contactus/contact.png";
 import "./contactus.css";
 
 const ContactUs = () => {
-  return (
-    <div className="contactus">
-       
-       <div className="head-container">
-        <p className="contact-big-text-one">Let's</p>
-        <p className="contact-big-text-two">Connect</p>
-       </div>
+	return (
+		<div className="contactus">
+			<div className="head-container">
+				<p className="contact-big-text-one">Let's</p>
+				<p className="contact-big-text-two">Connect</p>
+			</div>
 
+			<div className="text-container">
+				<div className="text-subcontainer">
+					<div className="sub-container">
+						<h2>
+							Let’s create something <br /> amazing together!
+						</h2>
+						<p className="contact-text">
+							Whether you’re looking to <br /> collaborate, revamp your <br />
+							brand, or just say hello, we’d <br /> love to hear from you.{" "}
+							<br />
+							Let’s bring your vision to life!
+						</p>
+					</div>
 
-      <div className="text-container">
-        <div className="sub-container">
-        <p className="contact-text-one">Let’s create something amazing together! </p>
-        <p className="second-text">Whether you’re looking to collaborate, revamp your brand, or just say hello, we’d love to hear from you. </p>
-        <p className="third-text">Let’s bring your vision to life!</p>
-        </div>
-
-        <div className="image-contact">
-        <img src={contactimg} alt="" />
-        </div>
-      </div>
-    </div>
-  );
+					<div className="img-container">
+						<div className="image-contact">
+							<img src={contactimg} alt="" />
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default ContactUs;
--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -5,52 +5,50 @@ import linkedin from "../../assets/footer/linkedin.jpg";
 import instagram from "../../assets/footer/instagram.jpg";
 
 const Footer = () => {
-  return (
-    <div className="footer-container">
-      <div className="footer-img-container">
-        <img className="footer-image" src={gulabologo} alt="" />
+	return (
+		<div className="footer-container">
+			<div className="footer-img-container">
+				<div>
+					<img className="footer-image" src={gulabologo} alt="" />
+					<div className="footer-social-icons">
+						<img src={instagram} alt="" />
+						<img className="footer-social-icon-two" src={linkedin} alt="" />
+					</div>
+				</div>
+			</div>
 
-        
-        <div className="footer-social-icons">
-          <img src={instagram} alt="" />
-          <img className="footer-social-icon-two" src={linkedin} alt="" />
-        </div>
-        
-      </div>
-     
+			<div className="footer-grid">
+				<div className="footer-grid-one">
+					<div className="footer-section-one">
+						<h3 className="footer-header">ABOUT</h3>
+						<p className="footer-subtext">About Gulabo</p>
+						<p className="footer-subtext">Team</p>
+					</div>
 
-      <div className="footer-grid">
-        <div className="footer-grid-one">
-          <div className="footer-section-one">
-            <h3 className="footer-header">ABOUT</h3>
-            <p className="footer-subtext">About Gulabo</p>
-            <p className="footer-subtext">Team</p>
-          </div>
+					<div className="footer-section-two">
+						<h3 className="footer-header">CONTACT</h3>
+						<p className="footer-subtext">hello@gulabodesign.com</p>
+						<p className="footer-subtext">+911294009373</p>
+					</div>
+				</div>
 
-          <div className="footer-section-two">
-            <h3 className="footer-header">CONTACT</h3>
-            <p className="footer-subtext">hello@gulabodesign.com</p>
-            <p className="footer-subtext">+911294009373</p>
-          </div>
-        </div>
+				<div className="footer-grid-two">
+					<div className="footer-section-third">
+						<h3 className="footer-header">SERVICES</h3>
+						<p className="footer-subtext">Experience Design</p>
+						<p className="footer-subtext">Uniform Design</p>
+						<p className="footer-subtext">Branding</p>
+					</div>
 
-        <div className="footer-grid-two">
-          <div className="footer-section-third">
-            <h3 className="footer-header">SERVICES</h3>
-            <p className="footer-subtext">Experience Design</p>
-            <p className="footer-subtext">Uniform Design</p>
-            <p className="footer-subtext">Branding</p>
-          </div>
-
-          <div className="footer-section-fourth">
-            <h3 className="footer-header">ADDRESS</h3>
-            <p className="footer-subtext">346, Sector 21A, Faridabad,</p>
-            <p className="footer-subtext">Haryana 121002</p>
-          </div>
-        </div>
-      </div>
-    </div>
-  );
+					<div className="footer-section-fourth">
+						<h3 className="footer-header">ADDRESS</h3>
+						<p className="footer-subtext">346, Sector 21A, Faridabad,</p>
+						<p className="footer-subtext">Haryana 121002</p>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default Footer;
